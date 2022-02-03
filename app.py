ae6ec3569d7566c7103a8b9cfc182232fd88ea8f
Gideon Tong <gideon@gideontong.com>
2022-02-02 18:21:00 -0800

    Reformat main app

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from json import load
 from rides.email.sender import send
-
-with open('config/keys.json') as fp:
-    config = load(fp)
-
-email = config['email']
-username = email['username']
-password = email['password']
-
-host = email['outbound']['host']
-port = email['outbound']['port']
-
-
-# send(host, port, username, password, '', 'this is a test message')
-
 from rides.email.receiver import retrieve
 
-inbound = config['email']['inbound']
+if __name__ == '__main__':
+    with open('config/keys.json') as fp:
+        config = load(fp)
 
-retrieve(inbound['host'], username, password)
+    email = config['email']
+    username = email['username']
+    password = email['password']
+
+    host = email['outbound']['host']
+    port = email['outbound']['port']
+
+    # send(host, port, username, password, '', 'this is a test message')
+
+    inbound = config['email']['inbound']
+
+    retrieve(inbound['host'], username, password)
