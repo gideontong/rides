e70d173f01cc91382ffe1dc2a460ae99cd274710
Gideon Tong <gideon@gideontong.com>
2022-02-02 18:19:26 -0800

    Working mailbox fetch

--- app.py
+++ app.py
@@ -2,12 +2,20 @@ from json import load
 from rides.email.sender import send
 
 with open('config/keys.json') as fp:
-    keys = load(fp)
+    config = load(fp)
 
-host = keys['email']['outbound']['host']
-port = keys['email']['outbound']['port']
-username = keys['email']['username']
-password = keys['email']['password']
+email = config['email']
+username = email['username']
+password = email['password']
+
+host = email['outbound']['host']
+port = email['outbound']['port']
 
 
-send(host, port, username, password, '', 'this is a test message')
+# send(host, port, username, password, '', 'this is a test message')
+
+from rides.email.receiver import retrieve
+
+inbound = config['email']['inbound']
+
+retrieve(inbound['host'], username, password)
