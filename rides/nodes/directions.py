4037479e2ec9113c1213f5480d6242305a76e2f5
Gideon Tong <gideon@gideontong.com>
2022-02-03 17:33:08 -0800

    Create directions

---
+++ rides/nodes/directions.py
@@ -0,0 +1,3 @@
+class location:
+    def __init__(self, address: str):
+        self.address = address
--- rides/util/chat.py
+++ rides/util/chat.py
@@ -1,4 +1,22 @@
 from re import sub
+from typing import Tuple
 
-def parse_yes_no(text: str) -> bool:
-    text = sub('[^A-Za-z]+', '1', text)
+
+def parse_yes_no(text: str) -> Tuple[bool, bool]:
+    '''Returns best guess at yes/no and 100% confidence level'''
+    text = sub('[^A-Za-z]+', ' ', text)
+    words = set(text.lower().split())
+
+    yes = 'yes' in words
+    no = 'no' in words
+
+    if (yes and no) or not (yes or no):
+        # TODO: Write a parse that determines minimum distance
+        return True, False
+    elif yes:
+        return True, True
+    elif no:
+        return False, True
+    else:
+        # TODO: Raise error as this should be unreachable
+        return False, False
