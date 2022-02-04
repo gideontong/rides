74110ef73b3592bb708389bf0b8ace04628489ce
Gideon Tong <gideon@gideontong.com>
2022-02-03 17:37:59 -0800

    Update messages

--- rides/nodes/__init__.py
+++ rides/nodes/__init__.py
@@ -1,4 +1,5 @@
 from rides.util import domains
+from rides.util.chat import parse_yes_no
 from typing import Tuple, Union
 
 
@@ -25,23 +26,33 @@ class person:
         self.last_step = self.step_can_drive
 
         if error:
-            return 'I didn\'t understand!', 'As a number, how many people can you take?'
-        return 'Thanks!', 'How many people are you able to take?'
+            return 'I didn\'t understand!', 'As a number, how many people can you take (do not include yourself)?'
+        return 'Thanks!', 'Not including yourself, how many people are you able to take?'
 
     def step_not_driving(self, mode: str, error: bool = False) -> Tuple[str, str]:
         self.last_step = self.step_not_driving
         
         return 'No worries.', 'We will try to find you a ride ASAP.'
     
-    def wrap_next_step(self, mode: str, error: bool = False, result: Union[str, None] = None) -> Tuple[str, str]:
+    def wrap_next_step(self, mode: str, result: Union[str, None] = None) -> Tuple[str, str]:
         if not self.last_step:
-            return self.step_start(mode, error)
+            return self.step_start(mode)
         
         elif self.last_step is self.step_start:
-            return self.step_can_drive(mode, error)
+            answer, confident = parse_yes_no(result)
+
+            if not confident:
+                return self.step_start(mode, error=True)
+            
+            if answer:
+                return self.step_can_drive(mode)
+            else:
+                return self.step_not_driving(mode)
         
         elif self.last_step is self.step_can_drive:
+            # TODO
             pass
 
         elif self.last_step is self.step_not_driving:
+            # TODO
             pass
